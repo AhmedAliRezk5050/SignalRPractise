be42a662f6a00bd6aaf2cdf55e9b05996bd8b8a2
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-31 03:28:31 +0200

    Client Add SignalR

--- SignalRPractise/Program.cs
+++ SignalRPractise/Program.cs
@@ -1,15 +1,17 @@
+using SignalRPractise.Hub;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CORSPolicy",
-        builder => builder
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials()
-        .SetIsOriginAllowed((hosts) => true));
+  options.AddPolicy("CORSPolicy",
+      builder => builder
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowCredentials()
+      .SetIsOriginAllowed((hosts) => true));
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,13 +23,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseCors("CORSPolicy");
 
 app.UseAuthorization();
+
+app.MapHub<MessageHub>("/offers");
 
 app.MapControllers();
 
